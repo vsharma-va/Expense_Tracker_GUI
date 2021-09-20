8962a6017e416fbe0e7c5189ac173885fcaf593a
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-09-20 21:42:38 +0530

    Added Proper Function Filters, Now user can change the amount column and it will be reflected in the csv file

--- User_Interface/main.py
+++ User_Interface/main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QDate, QEvent
+from PyQt5.QtCore import QDate, QEvent, Qt
 from PyQt5 import uic
 import sys
 import Src.Expenses
@@ -7,8 +7,8 @@ from PyQt5.QtChart import *
 
 
 cmbMainModeData = ['Yearly', 'Monthly', 'All Time']
-cmbMainMonthData = {'None'}
-cmbMainYearData = {'None'}
+cmbMainMonthData = ['None']
+cmbMainYearData = ['None']
 
 
 class AddExpenseWindow(QWidget):
@@ -94,6 +94,9 @@ class DlgMain(QMainWindow):
         self.btnRefreshFilters = self.findChild(QPushButton, "btnRefreshFilters")
         self.wiChart = self.findChild(QChartView, 'wiChart')
 
+        # flags
+        self.tblShowData.setWindowFlags(self.tblShowData.windowFlags() & Qt.ItemIsEditable)
+
         # calling functions
         self.loadData_tblShowData()
         self.loadData_cmbs()
@@ -103,6 +106,10 @@ class DlgMain(QMainWindow):
         self.btnAddExpense.clicked.connect(self.evt_btnAddExpense_clicked)
         self.btnRefreshFilters.clicked.connect(self.evt_btnRefreshFilters_clicked)
         self.cmbMode.currentIndexChanged.connect(self.updateGraphsAndTable)
+        self.cmbYear.currentIndexChanged.connect(self.updateGraphsAndTable)
+        self.cmbMonth.currentIndexChanged.connect(self.updateGraphsAndTable)
+        self.tblShowData.cellChanged.connect(self.evt_tblShowData_cellChanged)
+        self.tblShowData.cellChanged.connect(self.updateGraphsAndTable)
 
     # signal functions
     def evt_btnAddExpense_clicked(self):
@@ -115,25 +122,36 @@ class DlgMain(QMainWindow):
         self.cmbYear.clear()
         self.loadData_cmbs()
 
+    def evt_tblShowData_cellChanged(self, row, column):
+        self.tblShowData.blockSignals(True)
+        requiredRecord = self.tblShowData.item(row, column)
+        self.tblShowData.setItem(row, column, requiredRecord)
+        self.tblShowData.blockSignals(False)
+        obj = Src.Expenses.Expense()
+        obj.ChangeRecords(row, column, requiredRecord.text())
+
     # Display Items
     def loadData_tblShowData(self):
         try:
-            row = 0
-            column = 0
+            row1 = 0
+            column1 = 0
             allData = Src.Expenses.Expense.ReturnData()
+            self.tblShowData.blockSignals(True)
             self.tblShowData.setRowCount(len(allData))
             self.tblShowData.setColumnCount(3)
             self.tblShowData.setColumnWidth(2, 131)
             for li in allData:
                 for element in li:
-                    self.tblShowData.setItem(row, column, QTableWidgetItem(element))
-                    column += 1
-                column = 0
-                row += 1
+                    self.tblShowData.setItem(row1, column1, QTableWidgetItem(element))
+                    column1 += 1
+                column1 = 0
+                row1 += 1
+            self.tblShowData.blockSignals(False)
         except FileNotFoundError:
             QMessageBox.critical(self, 'Error', 'No Expense records exist!')
 
     def loadData_cmbs(self):
+        self.blockSignals(True)
         allData = Src.Expenses.Expense.ReturnData()
         counter = 0
         for li in allData:
@@ -141,12 +159,15 @@ class DlgMain(QMainWindow):
                 counter += 1
                 if counter == 1:
                     monthYear = element.split(' ')
-                    cmbMainMonthData.add(monthYear[0])
-                    cmbMainYearData.add(monthYear[1])
+                    if monthYear[0] not in cmbMainMonthData:
+                        cmbMainMonthData.append(monthYear[0])
+                    if monthYear[1] not in cmbMainYearData:
+                        cmbMainYearData.append(monthYear[1])
             counter = 0
         self.cmbMode.addItems(cmbMainModeData)
         self.cmbMonth.addItems(cmbMainMonthData)
         self.cmbYear.addItems(cmbMainYearData)
+        self.blockSignals(False)
 
     # Plots
     def displayGraph(self):
@@ -182,6 +203,7 @@ class DlgMain(QMainWindow):
             chart.setAxisX(xAxis, series)
 
             self.wiChart.setChart(chart)
+
         elif cmbMainModeData[self.cmbMode.currentIndex()] == 'Yearly':
             indexMonth = self.cmbMonth.findData('None')
             lengthMonth = self.cmbMonth.count()
@@ -192,6 +214,59 @@ class DlgMain(QMainWindow):
             for z in range(lengthYear):
                 self.cmbYear.model().item(z).setEnabled(True)
 
+            set0 = QBarSet("Expenses Yearly")
+
+            data = Src.Expenses.Expense.ReturnData()
+            dataToPlot = []
+            expenseList = []
+            tagsList = []
+            tempList = []
+            addToDataToPlot = False
+            counter = 0
+            for li in data:
+                for element in li:
+                    counter += 1
+                    if counter == 1:
+                        monthYear = element.split(' ')
+                        if monthYear[1] == cmbMainYearData[self.cmbYear.currentIndex()]:
+                            addToDataToPlot = True
+                    if addToDataToPlot:
+                        if counter != 1:
+                            tempList.append(element)
+                dataToPlot.append(tempList)
+                tempList = []
+                addToDataToPlot = False
+                counter = 0
+
+            anotherCounter = 0
+            for li2 in dataToPlot:
+                for element in li2:
+                    anotherCounter += 1
+                    if anotherCounter == 1:
+                        expenseList.append(float(element))
+                    elif anotherCounter == 2:
+                        tagsList.append(element)
+                anotherCounter = 0
+
+            set0.append(expenseList)
+
+            series = QBarSeries()
+            series.append(set0)
+
+            chart = QChart()
+            chart.addSeries(series)
+            chart.setTitle("")
+            chart.setAnimationOptions(QChart.AllAnimations)
+
+            categories = tagsList
+            xAxis = QBarCategoryAxis()
+            xAxis.append(categories)
+
+            chart.createDefaultAxes()
+            chart.setAxisX(xAxis, series)
+
+            self.wiChart.setChart(chart)
+
         elif cmbMainModeData[self.cmbMode.currentIndex()] == 'Monthly':
             lengthMonth = self.cmbMonth.count()
             lengthYear = self.cmbYear.count()
@@ -200,6 +275,58 @@ class DlgMain(QMainWindow):
             for z in range(lengthYear):
                 self.cmbMonth.model().item(z).setEnabled(True)
 
+            data = Src.Expenses.Expense.ReturnData()
+            dataToPlot = []
+            expenseList = []
+            tagsList = []
+            tempList = []
+            addToDataToPlot = False
+            counter = 0
+            for li in data:
+                for element in li:
+                    counter += 1
+                    if counter == 1:
+                        monthYear = element.split(' ')
+                        if monthYear[1] == cmbMainYearData[self.cmbYear.currentIndex()] and \
+                                monthYear[0] == cmbMainMonthData[self.cmbMonth.currentIndex()]:
+                            addToDataToPlot = True
+                    if addToDataToPlot:
+                        if counter != 1:
+                            tempList.append(element)
+                dataToPlot.append(tempList)
+                tempList = []
+                addToDataToPlot = False
+                counter = 0
+
+            anotherCounter = 0
+            for li2 in dataToPlot:
+                for element in li2:
+                    anotherCounter += 1
+                    if anotherCounter == 1:
+                        expenseList.append(float(element))
+                    elif anotherCounter == 2:
+                        tagsList.append(element)
+                anotherCounter = 0
+
+            set1 = QBarSet("Expenses Monthly")
+            set1.append(expenseList)
+
+            series = QBarSeries()
+            series.append(set1)
+
+            chart = QChart()
+            chart.addSeries(series)
+            chart.setTitle("")
+            chart.setAnimationOptions(QChart.AllAnimations)
+
+            categories = tagsList
+            xAxis = QBarCategoryAxis()
+            xAxis.append(categories)
+
+            chart.createDefaultAxes()
+            chart.setAxisX(xAxis, series)
+
+            self.wiChart.setChart(chart)
 
     # update widget functions:
     def updateGraphsAndTable(self):
@@ -213,7 +340,6 @@ class DlgMain(QMainWindow):
         elif event.type() == QEvent.WindowDeactivate:
             self.updateGraphsAndTable()
         return False
-
 
 
 def my_exception_hook(exctype, value, traceback):
